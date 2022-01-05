7acae6f5be59d6337fdab13824f18a8ffffa76dc
Andrew Hexen <SyberHexen@gmail.com>
2022-01-05 15:17:37 -0800

    kiev: lineage -> aosp
    Change-Id: Ib442cf426936b12899e2014206b4f8e0758fe4d7

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_kiev.mk
+    $(LOCAL_DIR)/aosp_kiev.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_kiev-eng \
-    lineage_kiev-user \
-    lineage_kiev-userdebug
+    aosp_kiev-eng \
+    aosp_kiev-user \
+    aosp_kiev-userdebug
