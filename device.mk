f8a01475a5ea3fa44d74b3e8061918154c0d4a94
Andrew Hexen <SyberHexen@gmail.com>
2022-04-03 05:59:50 -0700

    kiev: PE: drop livedisplay
    * Support is dropped in PE Source
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I64c77993514959851ed93e0881247eb39728bd45

--- device.mk
+++ device.mk
@@ -376,11 +376,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.kiev
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs.motorola_lito
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
