73ece0d1fb7f8880cccdd764387faa73d6e05714
Patrick Gaskin <patrick@pgaskin.net>
2022-09-29 19:03:49 -0400

    kiev: add ADB vendor keys
    Change-Id: Icfc20557243d748e0890a2ffcda34d5b59a163dc

--- lineage_kiev.mk
+++ lineage_kiev.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/motorola/kiev/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# ADB vendor keys
+$(call inherit-product, vendor/pgaskin/adb/vendorkey.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_kiev
 PRODUCT_DEVICE := kiev
